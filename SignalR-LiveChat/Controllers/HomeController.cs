fa1297e9b3ce9259740fcbc2bcc98fe9758b1503
Marco041994 <75069311+Marco041994@users.noreply.github.com>
2021-02-21 22:02:57 +0100

    Upload Project

---
+++ SignalR-LiveChat/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalR_LiveChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "LiveChat mit SignalR für ein Schulprojekt im Fach Netzwerksysteme";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
